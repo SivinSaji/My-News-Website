dfe33cb24286a16914f817915e5cf20c9aceef8c
Sivin Saji <sivinsaji22@gmail.com>
2022-06-22 10:25:10 +0530

    Added news search option and categry option

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+
+
 const app = express()
 const port = 5000
 
 
 //Static Files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 app.use('/css',express.static(__dirname + 'public/css'))
 app.use('/img',express.static(__dirname + 'public/img'))
 app.use('/js',express.static(__dirname + 'public/js'))
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const newsRouter = express.Router()
 const axios = require('axios')
-
+const moment = require('moment')
+const math = require('math')
 newsRouter.get('', async(req,res)=>{
    // res.render('news')
 
@@ -12,7 +13,6 @@ newsRouter.get('', async(req,res)=>{
     'apiKey=81616514c8534374bf15ce029ff96550';
 
   const newsAPI =await axios.get(url)
-  console.log(newsAPI.data.articles);
   res.render('news',{articles : newsAPI.data.articles})
    }catch(err){
       if(err.respo){
@@ -29,11 +29,12 @@ newsRouter.get('', async(req,res)=>{
    }
 
 })
+/*
+newsRouter.get('/search',async(req,res)=>{
 
-newsRouter.post('/search',async(req,res)=>{
-  const search=req.body.search;
-  console.log(req.body.search);
-
+  var search=req.query.name;
+  console.log(search);
+  console.log("oooooooooooooooooooooooooo   erjeggen bg");
   try{
     var url = `http://newsapi.org/v2/everything?q=${search}&apiKey=81616514c8534374bf15ce029ff96550`
 
@@ -45,8 +46,42 @@ newsRouter.post('/search',async(req,res)=>{
     }
 
 }
+})*/
+newsRouter.post('/search',async(req,res)=>{
+ 
+  search = req.body.search;
+
+
+  try {
+      var url = `http://newsapi.org/v2/everything?q=${search}&apiKey=81616514c8534374bf15ce029ff96550`
+
+      const news_get =await axios.get(url)
+      res.render('news',{articles:news_get.data.articles})
+
+
+
+
+
+  } catch (error) {
+      if(error.response){
+          console.log(error)
+      }
+
+  }
 })
 
+newsRouter.get('/news/:category',async(req,res)=>{
+  var category = req.params.category;
+  try{
+    var url = 'http://newsapi.org/v2/top-headlines?country=in&category=' + category + '&apiKey=81616514c8534374bf15ce029ff96550';
 
+    const newsAPI =await axios.get(url)
+    res.render('category',{articles:newsAPI.data.articles})
+  }catch(error){
+    if(error.response){
+      console.log(error);
+    }
+  }
+})
 
 module.exports = newsRouter
